b28bd0f543189351bcba89ffe478ed42119d9ad4
Derek Chen <53978637+DereC4@users.noreply.github.com>
2021-06-02 16:20:27 -0500

    Terraria mods are cool

---
+++ Items/StoneSword.cs
@@ -0,0 +1,44 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MinecraftSwords.Items
+{
+	public class StoneSword : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			DisplayName.SetDefault("Stone Sword");
+			// By default, capitalization in classnames will add
+			// spaces to the display name. You can customize the
+			// display name here by uncommenting this line.
+			Tooltip.SetDefault("We didn't realise we were making memories, we just knew we were having fun...");
+		}
+
+		public override void SetDefaults() 
+		{
+			item.damage = 150;
+			item.melee = true;
+			item.width = 40;
+			item.height = 40;
+			item.useTime = 16;
+			item.useAnimation = 20;
+			item.useStyle = 1;
+			item.knockBack = 6;
+			item.value = 10000;
+			item.rare = 2;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+		}
+
+		public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Meowmere, 1);
+			recipe.AddIngredient(ItemID.StoneBlock, 2);
+			recipe.AddIngredient(ItemID.Wood, 1);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
---
+++ MinecraftSwords.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace MinecraftSwords
+{
+	public class MinecraftSwords : Mod
+	{
+	}
+}
